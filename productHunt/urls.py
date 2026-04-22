e9afb68dd638889478d91a7ff9416e908db5cdef
ShathaMBahassan <92083449+Shathamhb@users.noreply.github.com>
2026-04-22 15:19:01 +0300

    fisrt commit

---
+++ productHunt/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path , include 
+from django.conf.urls.static import static 
+from django.conf import settings
+import Users.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('Users/',include('Users.urls')),
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
