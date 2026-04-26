690c28ce380316de16cf742bac31ca67abaea69f
ShathaMBahassan <92083449+Shathamhb@users.noreply.github.com>
2026-04-26 14:51:43 +0300

    Add Create_User model with username, full_name, and password fields

--- Users/admin.py
+++ Users/admin.py
@@ -1,1 +1,4 @@
 from django.contrib import admin
+from .models import Create_User
+
+admin.site.register(Create_User)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class Create_User(models.Model):
+    username = models.CharField(max_length=200)
+    full_name = models.CharField(max_length=200)
+    Password = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"the user is {self.full_name}"
