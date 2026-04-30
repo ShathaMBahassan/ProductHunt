bf48bbb2cb99a0d1fea5f0afec7ede15a2b957ca
ShathaMBahassan <92083449+Shathamhb@users.noreply.github.com>
2026-04-30 09:40:06 +0300

    add logo

--- productHunt/settings.py
+++ productHunt/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "procductHunt",
         "USER": "postgres",
-        "PASSWORD": "",
+        "PASSWORD": "bahassan@ils",
         "HOST": "localhost",
         "PORT": "5432",
     }
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
