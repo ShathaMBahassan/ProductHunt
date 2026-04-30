28d2e00a60c92458e74b4993f6fb393d9262606e
ShathaMBahassan <92083449+Shathamhb@users.noreply.github.com>
2026-04-30 11:49:33 +0300

    Develop Sing Up User

--- Users/urls.py
+++ Users/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path("login/",views.login,name='login_page')
+    path("login/",views.login,name='login_page'),
+    path("signUp/",views.signUp,name="singUp_page"),
+    path("logOut/",views.logOut,name="logOut_page")
 ] 
--- Users/views.py
+++ Users/views.py
@@ -1,8 +1,38 @@
 from django.shortcuts import render
+from .models import Create_User
 
 
 def login(request):
 
     return render(request,'login.html')
 
-# Create your views here.
+
+def signUp(request):
+    if request.method =='POST':
+        # User enter the info
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = Create_User.objects.get(username=request.POST['username'])
+                user_message = "the user has been already exist"
+                return render(request,'signUp.html',{'error_user':user_message})
+            except Create_User.DoesNotExist:
+                user = Create_User.objects.create(
+                    username= request.POST['username'],
+                    Password = request.POST['password1']
+                )
+                return render(request,'message.html')
+        else:
+            message = "Password and confirmation do not match."
+            return render(request,'signUp.html', {'error_password': message})
+
+    else:
+        # User didn't add info
+        return render(request,'signUp.html')
+
+
+
+def logOut(request):
+
+    return render(request,'logOut.html')
+
+
